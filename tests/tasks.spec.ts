3ba73cb27d2de7f2b7fd35f2748f0b8112d0bb2e
Vitor Moura <vitorlucenam@gmail.com>
2024-04-11 18:18:38 -0300

    Adicionando comentários

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -5,11 +5,13 @@ import { deleteTaskByHelper, postTask } from './support/helpers'
 import { TasksPage } from './pages/tasks'
 
 test('Cadastrar uma nova tarefa', async ({ page, request }) => {
-
+    // Masa de teste
     const task: TaskModel = { name: "Comprar o curso do papito", is_done: false }
 
+    // Chamadas na API
     await deleteTaskByHelper(request, task.name)
 
+    // Cenário de teste
     const tasksPage: TasksPage = new TasksPage(page)
     await tasksPage.go()
     await tasksPage.create(task)
@@ -18,14 +20,14 @@ test('Cadastrar uma nova tarefa', async ({ page, request }) => {
 });
 
 test('Não deve permitir cadastrar tarefa repetida', async ({ page, request }) => {
-
+    // Masa de teste
     const task: TaskModel = { name: "Estudar Playwright", is_done: false }
 
-    // Chamadas na API para: Fazer o reset da massa, realizar o cadastro, verificar se o status recebido foi true
+    // Chamadas na API
     await deleteTaskByHelper(request, task.name)
     await postTask(request, task)
 
-    // Teste em si
+    // Cenário de teste
     const tasksPage: TasksPage = new TasksPage(page)
     await tasksPage.go()
     await tasksPage.create(task)
