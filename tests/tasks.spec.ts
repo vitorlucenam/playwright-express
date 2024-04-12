1a4fb354578609f623b7c521dc913ef1103c9c06
Vitor Moura <vitorlucenam@gmail.com>
2024-04-12 16:50:40 -0300

    Adicionando mais um cenário de validação

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 import { TaskModel } from './fixtures/task.model'
 import { deleteTaskByHelper, postTask } from './support/helpers'
@@ -33,4 +33,16 @@ test('Não deve permitir cadastrar tarefa repetida', async ({ page, request }) =
     await tasksPage.create(task)
     await tasksPage.alertHaveText('Task already exists!')
 
+});
+
+test   ('Não deve permitir criar tarefa vazia', async ({ page }) => {
+    // Masa de teste
+    const task: TaskModel = { name: "", is_done: false }
+
+    // Cenário de teste
+    const tasksPage: TasksPage = new TasksPage(page)
+    await tasksPage.go()
+    await tasksPage.create(task)
+    const validationMessage = await tasksPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
+    expect(validationMessage).toEqual('This is a required field')
 });
